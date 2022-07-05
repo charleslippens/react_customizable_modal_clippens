235bbc39b73b28c1c182c8674431f4f0b557a8bd
CL <72822672+charleslippens@users.noreply.github.com>
2022-07-05 10:25:54 +0200

    update modal

---
+++ dist/component/modal.js
@@ -0,0 +1,26 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./styling/modal.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Modal(element) {
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "Theme"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "Message"
+  }, /*#__PURE__*/_react.default.createElement("p", null, element.text), /*#__PURE__*/_react.default.createElement("button", {
+    className: "Button",
+    onClick: element.close
+  }, "X")));
+}
+
+var _default = Modal;
+exports.default = _default;
