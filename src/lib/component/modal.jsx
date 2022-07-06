39b2d78dc5ab83954c4ae6cee4348e0cdf2bc28c
CL <72822672+charleslippens@users.noreply.github.com>
2022-07-06 10:24:10 +0200

    update modal

--- src/lib/component/modal.jsx
+++ src/lib/component/modal.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import "./Styling/modal.css";
+import "../styling/modal.css";
 
 function Modal(element) {
 	return (
-		<div className="Theme">
-			<div className="Background">
-				<div className="Message">
-					<p>{element.text}</p>
-					<button className="Button" onClick={element.close}>
-						X
-					</button>
-				</div>
+		<div className="Background">
+			<div className="Message">
+				<p>{element.text}</p>
+				<button className="Button" onClick={element.close}>
+					X
+				</button>
 			</div>
 		</div>
 	);
